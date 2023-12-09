4598a6f1d74e237300ca978557973e20a4cfba7f
Ian Lumsden <lumsden.ian@gmail.com>
2023-12-08 19:47:53 -0800

    Sets CALI_RECORDER_FILENAME in the run template script so that we can (hopefully) get meaningful file names for multi-client runs

--- run/run_template.sh
+++ run/run_template.sh
@@ -7,12 +7,12 @@
 server_ip=$(flux exec -r 0 hostname -i)
 
 flux submit -N 1 --exclusive --tasks-per-node=1 --requires=rank:0 \
-    --env=CALI_SERVICES_ENABLE="event,trace,recorder,timestamp" \
+    --env=CALI_SERVICES_ENABLE="event,trace,recorder,timer" \
     --output=server.out --error=server.err \
     {{ server_exe }} tag $server_ip {{ data_size }} {{ num_clients }} -p 8888
 
 flux submit -N 1 --exclusive --tasks-per-node={{ num_clients }} --label-io \
-    --env=CALI_SERVICES_ENABLE="event,trace,recorder,timestamp" \
+    --env=CALI_SERVICES_ENABLE="event,trace,recorder,timer" --env=CALI_RECORDER_FILENAME="client-%mpi.rank%.cali" \
     --output=clients.out --error=clients.err \
     {{ client_exe }} tag $server_ip {{ data_size }} -n {{ num_iters }} -p 8888
         
